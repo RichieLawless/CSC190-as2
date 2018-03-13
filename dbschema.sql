1929569df93961bfd08feda293aee0dd9e5ba09e
CSC190 GitAccount <csc190Spr2017@gmail.com>
2018-03-12 20:21:38 -0400

    commit after trying anything to get this to work...

--- dbschema.sql
+++ dbschema.sql
@@ -1,7 +1,8 @@
 DROP DATABASE IF EXISTS db_users;
 CREATE DATABASE db_users;
 USE db_users;
-CREATE TABLE tbl_users(
+
+$ sql = CREATE TABLE tbl_users(
 	uname varchar(255) primary key,
 	uname varchar(1024),
 	message varchar(1024)
